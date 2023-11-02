83105ba5e4b4fae6659e5da5510cec0df96dcd65
PraveenaSasanamu68 <141649464+PraveenaSasanamu68@users.noreply.github.com>
2023-11-02 11:53:25 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+// external dependencies
+import React from 'react';
+/** This is a functional component for creating the footer of the application
+ */
+function Footer() {
+
+	return (
+		<div className="footer">
+			<p>Here is the footer</p>
+		</div>
+	);
+}
+export default Footer;
